fd2244efc73e1bdfa6a105ccdb54cf091f41a94c
Pavel Shvarchov <Shvarpa@gmail.com>
2018-12-12 08:43:56 +0200

    restructuring to different js files

--- js/gui.js
+++ js/gui.js
@@ -1,9 +1,14 @@
 class GUI{
-	constructor(element){
-		this.container = element
+	constructor(body,title,fields={}){
+		this.title = title
+		this.container = document.createElement("div")
+		let titleHeader = document.createElement("h2")
+		titleHeader.innerHtml = title
+		this.form = document.createElement("form")
+		this.form.id = "form_" + this.title;
 	}
-	this.fields = {}
-	addField(field_name,type){
+	
+	addField(field_name,type,form = this.form){
 		if !(field_name in this.fields{}){
 			this.fields[field_name] = document.createElement("div")
 			this.fields[field_name].className = "form-inline"
@@ -20,9 +25,35 @@ class GUI{
 		input.className = "form-control"
 		input.id = field_name
 		field.appendChild(input)
+		
+		form.appendChild(field)
+	}
+	
+	addSubmit(text = "Submit"){
+		if(this.button) {return}
+		this.button = document.createElement("button")
+		this.button.type = "submit"
+		this.button.className = "btn btn-default"
+		this.button.innerHTML = text
+		this.container.appendChild(this.button)
 	}
 	
 	selectMicroservice(service){
 		structure = service.help()
 	}
 }
+
+<div class="jumbotron text-center">
+  <h2>Calculator Microservices</h2>
+  <form id="form">
+    <div class="form-inline">
+      <label for="parameter 1">Parameter 1:</label>
+      <input type="text" class="form-control" id="parameter 1" placeholder="1">
+    </div>
+    <div class="form-inline">
+      <label for="parameter 2">Parameter 2:</label>
+      <input type="text" class="form-control" id="parameter 2" placeholder="2">
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+</div>
